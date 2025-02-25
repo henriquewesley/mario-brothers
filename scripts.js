21d4c9b810ea478bb992a685d7c7eb2fbb8c18c3
henriquewesley <whenfeof@gmail.com>
2025-02-25 15:49:07 -0300

    Site dos irmãos Mário e Luigi encanadores

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function mostrarFormulario() {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function esconderFormulario() {
+    form.style.left = "-309px"
+    form.style.transform = "translateX(0%)"
+    mascara.style.visibility = "hidden"
+}
